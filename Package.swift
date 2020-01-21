63fe2a65dacd785fe3d0c513f5f0c202c0f836f2
Kyle Jessup <kyle.jessup@treefrog.ca>
2020-01-21 11:04:58 -0500

    added platform spec to Package file

--- Package.swift
+++ Package.swift
@@ -5,6 +5,9 @@ import PackageDescription
 
 let package = Package(
 	name: "PerfectCRUD",
+	platforms: [
+		.macOS(.v10_15),
+	],
 	products: [
 		.library(name: "PerfectCRUD", targets: ["PerfectCRUD"])
 	],
